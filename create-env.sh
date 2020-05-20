179805683245234fdd54931f1f590dd2a6e0d598
AldoEstebanPaz <estebanpazutn@gmail.com>
2020-05-20 12:05:00 -0300

    disabled replace

--- create-env.sh
+++ create-env.sh
@@ -20,8 +20,8 @@ env
 # replace_env "MONGO_USER_NAME" ${dist_env_file}
 # replace_env "MONGO_USER_PASS" ${dist_env_file}
 
-dist_env_file="dist/functions/api.js"
-replace_js "process.env.MONGO_DB_URL" "MONGO_DB_URL" ${dist_env_file}
-replace_js "process.env.MONGO_DB_NAME" "MONGO_DB_NAME" ${dist_env_file}
-replace_js "process.env.MONGO_USER_NAME" "MONGO_USER_NAME" ${dist_env_file}
-replace_js "process.env.MONGO_CONNECTION_STRING" "MONGO_CONNECTION_STRING" ${dist_env_file}
+# dist_env_file="dist/functions/api.js"
+# replace_js "process.env.MONGO_DB_URL" "MONGO_DB_URL" ${dist_env_file}
+# replace_js "process.env.MONGO_DB_NAME" "MONGO_DB_NAME" ${dist_env_file}
+# replace_js "process.env.MONGO_USER_NAME" "MONGO_USER_NAME" ${dist_env_file}
+# replace_js "process.env.MONGO_CONNECTION_STRING" "MONGO_CONNECTION_STRING" ${dist_env_file}
