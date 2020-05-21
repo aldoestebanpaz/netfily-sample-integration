f0e31ee2089d890935838d310ac13e23c30cf1cf
AldoEstebanPaz <estebanpazutn@gmail.com>
2020-05-21 14:47:29 -0300

    update build without express

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 We will need to set all the environment variables listed in /backend/.env.sample in the netlify settings panel in order to be able to connect to the mongo database or other services.
+
+If you want to start the backend from here, you will need to set the environment variables in the system or creating a .env file in the root of this repo.
--- src/api.js
+++ src/api.js
@@ -1,13 +1,4 @@
-require('../backend/config');
-require('../backend/config-database');
-
-const express = require('express');
 const serverless = require('serverless-http');
-const apiV1Router = require('../backend/src/api/v1');
+const app = require('../backend/src/app').createExpressApp('/.netlify/functions');
 
-const app = express();
-app.use(express.json());
-
-app.use('/.netlify/functions' + '/api/v1', apiV1Router);
-
-module.exports.handler = serverless(app);
+exports.handler = serverless(app);
